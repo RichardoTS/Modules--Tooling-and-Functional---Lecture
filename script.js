5feb517b73d47bdc163cddda8ff6eab2d09e29f0
Ricardo <ri.tapiasaez@gmail.com>
2024-08-14 17:48:46 -0400

    Reviewing

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // addToCart("bread", 5);
 // console.log(price, tq);
 
-console.log("Importing module");
+// console.log("Importing module");
 
 // import * as ShoppingCart from "./shoppingCart.js";
 // ShoppingCart.addToCart("bread", 5);
@@ -12,12 +12,12 @@ console.log("Importing module");
 // import add, { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
 // console.log(price);
 
-import add, { cart } from "./shoppingCart.js";
-add("pizza", 2);
-add("bread", 5);
-add("apples", 4);
+// import add, { cart } from "./shoppingCart.js";
+// add("pizza", 2);
+// add("bread", 5);
+// add("apples", 4);
 
-console.log(cart);
+// console.log(cart);
 
 //////////////////////////////
 // Top-Level await
@@ -45,4 +45,31 @@ console.log(cart);
 // const lastPost2 = await getLastPost();
 // console.log(lastPost2);
 
-(function () {})();
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(
+//       `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+//     );
+//   };
+
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} order from supplier`);
+//   };
+
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+// ShoppingCart2.addToCart("apple", 4);
+// ShoppingCart2.addToCart("pizza", 2);
+// console.log(ShoppingCart2);
